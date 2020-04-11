7ead12ea7c5f8b6a87b587cc622c5dab88f8c0f2
adahyto <adahy.to@gmail.com>
2020-04-11 21:09:48 +0200

    apiURL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     agmApiKey: 'AIzaSyD5goHN6mmCS7RtkURKoMlzALm_D6lPT70',
-    apiUrl: 'https://shop-hub.herokuapp.com/',
+    apiUrl: 'https://shop-hub.herokuapp.com',
     production: false,
 };
